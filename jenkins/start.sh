82f119221d8f4ad77bac7e2455a996900ab688dc
lloyd <lloyd@opensourceinfrastructure.com>
2014-04-26 04:40:26 -0500

    add more logic

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -1,3 +1,22 @@
 source .bashrc
+TIMEOUT=60
+## clearing out jenkins log each time
+rm -f jenkins.log
 java -jar jenkins/jenkins.war > jenkins.log 2>&1 &
+while true
+do
+   sleep 5
+   TIME=$(expr ${TIME} + 5)
+   if [ $(grep -c 'INFO: Jenkins is fully up and running' jenkins.log) -gt 0 ];
+   then
+      echo "Jenkins fully up"
+      exit 0
+   fi
+   if [ ${TIME} -ge ${TIMEOUT} ]
+   then
+      echo "Timeout of ${TIMEOUT} exceed.  Outputing last 50 lines of jenkins.log"
+      tail -50 jenkins.log
+      exit 2
+   fi
+done
 
--- jenkins/stop.sh
+++ jenkins/stop.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-
+TIMEOUT=60
 ## stop
 java -jar jenkins/jenkins-cli.jar -s http://localhost:8080 shutdown >/dev/null 2>&1
+
+while true
+do
+   sleep 5
+   TIME=$(expr ${TIME} + 5)
+   if [ $(ps auxww | grep jenkins | grep -v grep | wc -l) -eq 0 ];
+   then
+      echo "Jenkins down"
+      exit 0
+   fi
+   if [ ${TIME} -ge ${TIMEOUT} ]
+   then
+      echo "Timeout of ${TIMEOUT} exceed.  Killing forcefully."  
+      kill -9 $(ps auxww | grep jenkins | grep -v grep | awk '{print $2}')
+      exit 2
+   fi   
+done
