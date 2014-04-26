bd1e6486a90f5b1adacf9ae28c5ede82ff48c46c
lloyd <lloyd@opensourceinfrastructure.com>
2014-04-26 05:26:39 -0500

    more

--- jenkins/configure.sh
+++ jenkins/configure.sh
@@ -4,13 +4,16 @@ pushd jenkins
 curl -s -L -O http://localhost:8080/jnlpJars/jenkins-cli.jar
 ## install plugins
 ## order matters with installation due to dependencies
-PLUGINS="github-api git-client scm-api git ghprb greenballs token-macro email-ext postbuildscript dashboard-view"
+PLUGINS="github-api github git-client scm-api git ghprb greenballs token-macro email-ext postbuildscript dashboard-view"
 
 for PLUGIN in ${PLUGINS}
 do
    echo "Installing plugin ${PLUGIN}"
    java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin ${PLUGIN}.hpi
 done
+
+echo "Copying over main config.xml"
+cp ../config.xml config.xml
 
 echo "Restarting Jenkins"
 java -jar jenkins-cli.jar -s http://localhost:8080 restart
